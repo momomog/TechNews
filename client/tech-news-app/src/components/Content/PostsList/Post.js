e1f1eaa4a3c4a9c0e33b58b6bd12ea39c529bf47
Sergey Fokin <momomogggq@gmail.com>
2019-11-11 22:29:52 +0300

    posts

---
+++ client/tech-news-app/src/components/Content/PostsList/Post.js
@@ -0,0 +1,81 @@
+import React from 'react';
+
+function Post() {
+    return (
+        <div className="well">
+            <div className="media">
+                <a className="pull-left" href="#">
+                    <img className="media-object" src="http://s.4pda.to/rvgGmYc5LQQviIz2z010z1FjoDTj0fWz2Ah7cZ6h.png" width="150" height="100"/>
+                </a>
+                <div className="media-body">
+                    <h4 className="media-heading">Kospet Prime — полноценный смартфон в корпусе часов</h4>
+                    <p className="text-right">Admin Admin</p>
+                    <p>Большинство умных часов позиционируется как дополнительный аксессуар, позволяющий
+                        просматривать входящие сообщения и отслеживать физическую активность. В случае с Kospet
+                        Prime речь идёт о полноценном смартфоне на запястье. В них есть сразу две камеры, огромный
+                        аккумулятор, Android со всеми нужными функциями и возможность установки SIM-карты. </p>
+                    <ul className="list-inline list-unstyled">
+                        <li><span><i className="glyphicon glyphicon-calendar"></i> 2 дня, 8 часов </span>
+                        </li>
+                        <li>|</li>
+                        <span><i className="glyphicon glyphicon-comment"></i> 2 комментария</span>
+                        <li>|</li>
+                        <li>
+                            <span className="glyphicon glyphicon-star"></span>
+                            <span className="glyphicon glyphicon-star"></span>
+                            <span className="glyphicon glyphicon-star"></span>
+                            <span className="glyphicon glyphicon-star"></span>
+                            <span className="glyphicon glyphicon-star-empty"></span>
+                        </li>
+                        <li>|</li>
+                        <li>
+                            <span><i className="fa fa-facebook-square"></i> </span>
+                            <span><i className="fa fa-twitter-square"></i> </span>
+                            <span><i className="fa fa-google-plus-square"></i> </span>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Post;
+
+{/*<div className="well">*/}
+{/*    <div className="media">*/}
+{/*        <a className="pull-left" href="#">*/}
+{/*            <img className="media-object" src="http://s.4pda.to/rvgGmwMz2QXmOVCiJAIpo8h2Sk6cuZySJacli.jpg" width="150" height="150"/>*/}
+{/*        </a>*/}
+{/*        <div className="media-body">*/}
+{/*            <h4 className="media-heading">Стала известна ключевая характеристика Samsung Galaxy S11</h4>*/}
+{/*            <p className="text-right">Admin Admin</p>*/}
+{/*            <p>*/}
+{/*                Вслед за первыми слухами о модели процессора будущего флагмана Samsung в сети появилась*/}
+{/*                более точная информация о модели процессора, который получит готовящийся к релизу Galaxy S11.*/}
+{/*                Новая утечка раскрыла модель CPU, на базе которого будет построен ещё не анонсированный*/}
+{/*                гаджет.*/}
+{/*            </p>*/}
+{/*            <ul className="list-inline list-unstyled">*/}
+{/*                <li><span><i className="glyphicon glyphicon-calendar"></i> 5 дней, 2 часа </span>*/}
+{/*                </li>*/}
+{/*                <li>|</li>*/}
+{/*                <span><i className="glyphicon glyphicon-comment"></i> 3 комментария</span>*/}
+{/*                <li>|</li>*/}
+{/*                <li>*/}
+{/*                    <span className="glyphicon glyphicon-star"></span>*/}
+{/*                    <span className="glyphicon glyphicon-star"></span>*/}
+{/*                    <span className="glyphicon glyphicon-star"></span>*/}
+{/*                    <span className="glyphicon glyphicon-star-empty"></span>*/}
+{/*                    <span className="glyphicon glyphicon-star-empty"></span>*/}
+{/*                </li>*/}
+{/*                <li>|</li>*/}
+{/*                <li>*/}
+{/*                    <span><i className="fa fa-facebook-square"></i> </span>*/}
+{/*                    <span><i className="fa fa-twitter-square"></i> </span>*/}
+{/*                    <span><i className="fa fa-google-plus-square"></i> </span>*/}
+{/*                </li>*/}
+{/*            </ul>*/}
+{/*        </div>*/}
+{/*    </div>*/}
+{/*</div>*/}
---
+++ client/tech-news-app/src/components/Content/PostsList/PostsList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Post from "./Post";
+
+function PostsList() {
+    return (
+        <div>
+            <Post/>
+            <Post/>
+        </div>
+    )
+}
+
+export default PostsList;

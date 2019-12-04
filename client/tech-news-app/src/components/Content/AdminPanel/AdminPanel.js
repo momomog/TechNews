d24b59bb3c8c359c0a3a30846b8bdbad2199240e
Sergey Fokin <momomogggq@gmail.com>
2019-12-04 18:26:30 +0400

    редактирование поста

--- client/tech-news-app/src/components/Content/AdminPanel/AdminPanel.js
+++ client/tech-news-app/src/components/Content/AdminPanel/AdminPanel.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {NavLink} from "react-router-dom";
 
 function AdminPanel() {
     return (
@@ -13,12 +14,48 @@ function AdminPanel() {
                                     <div className="col-sm-2"/>
                                     <h5 className="card-header w-100">Управление постами</h5>
                                         <div className="row">
-                                            <div className="col">
+                                            <div className="col-lg-4">
+                                                <div className="pl-4">
+                                                    <span>Добавить новый пост</span>
+                                                    <input id="login-username" type="text" className="admin-post-input ml-3 mt-3"
+                                                           name="usernameOrEmail"
+                                                        // onChange={this.onLoginFieldsChange}
+                                                           placeholder="ID поста"
+                                                           required/>
+
+                                                </div>
+
+                                                <div className="pl-4">
+                                                    <span>Удалить пост</span>
+                                                    <input id="login-username" type="text" className="admin-post-input ml-3 mt-3"
+                                                           name="usernameOrEmail"
+                                                        // onChange={this.onLoginFieldsChange}
+                                                           placeholder="ID поста"
+                                                           required/>
+                                                    <NavLink to={'/edit'}>
+                                                        <button type="button" className="btn btn-success ml-3">Добавить</button>
+                                                    </NavLink>
+                                                </div>
+
+                                                <div className="pl-4">
+                                                    <span>Редактировать пост</span>
+                                                    <input id="login-username" type="text" className="admin-post-input ml-3 mt-3"
+                                                           name="usernameOrEmail"
+                                                        // onChange={this.onLoginFieldsChange}
+                                                           placeholder="ID поста"
+                                                           required/>
+                                                    <NavLink to={'/edit'}>
+                                                        <button type="button" className="btn btn-success ml-3">Добавить</button>
+                                                    </NavLink>
+                                                </div>
 
                                             </div>
-                                            <div className="col">
-
+                                            <div className="col-lg-8">
+                                                <NavLink to={'/edit'}>
+                                                    <button type="button" className="btn btn-success ml-3">Добавить</button>
+                                                </NavLink>
                                             </div>
+
                                         </div>
 
 
